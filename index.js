6faa88275379e19d6cc87a3c825f676a661664e4
pavex <pavex@ines.cz>
2017-02-06 15:54:51 +0100

    Beta version of Publixe HTTP/REST client

---
+++ index.js
@@ -0,0 +1,14 @@
+import Request from './lib/Request.js';
+import RestRequest from './lib/RestRequest.js';
+
+
+/**
+ */
+module.exports = {
+
+// Network XHR
+	Request: Request,
+
+// REST single request client
+	RestRequest: RestRequest
+};
